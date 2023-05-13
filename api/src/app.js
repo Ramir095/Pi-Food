41812190be752a95f366fdc7b1b95b40c07dab37
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-05-12 21:49:00 -0300

    chore:update

--- api/src/app.js
+++ api/src/app.js
@@ -23,13 +23,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors(corsOptions))
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000/', 'https://pi-food-gray.vercel.app'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+
 
 server.use('/recipes', routesRecipes);
 server.use('/diets', routesDiets);
